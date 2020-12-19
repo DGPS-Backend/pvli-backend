6d70cacb0e4bedbefcbe5540b2d5b38b75ae188f
borjawer <74541082+transexylvania@users.noreply.github.com>
2020-12-20 00:38:59 +0100

    supongo que el equivalente a lo que estoy haciendo en piton es un asesinato. store level mennnn

--- app/daos/daoLevels.py
+++ app/daos/daoLevels.py
@@ -2,4 +2,11 @@ import mongoengine as me
 
 class Level(me.Document):
     id_level = me.StringField(primary_key=True)
+    name_level = me.StringField(required=True)
     json_phaser = me.StringField(required=True)
+    # Puntuaciones de todos los usuarios
+    rates = me.StringField(required=True)
+    # Comentarios de todos los usuarios
+    comments = me.StringField(required=True)
+    # Almacena si el nivel ha sido bloqueado
+    blocked = me.StringField(required=True)
--- app/modulo_levels/levels.py
+++ app/modulo_levels/levels.py
@@ -5,6 +5,9 @@ from daos.daoLevels import Level
 from mongoengine.errors import NotUniqueError
 from mongoengine.errors import ValidationError
 from pymongo.errors import ServerSelectionTimeoutError
+from random import randint
+
+max_level_id = 999999999999
 
 levels = Blueprint("levels", __name__)
 
@@ -29,8 +32,26 @@ def loadLevel():
 
 @levels.route('/storeLevel', methods=['POST'])
 def storeLevel():
+
     print("/storeLevel RECIBE", request.get_json())
-    return jsonify({"return_code": "200"}), 200
+
+    data = request.get_json()
+    response = jsonify({"return_code": 400, "message": "Solicitud incorrecta"}), 400
+
+    if ("name_level" in data) and ("json" in data):
+        # Genera ID único
+        id_level = randint(0, max_level_id)
+
+        try:
+            level = Level(id_level=id_level, name_level=data["name_level"], json_phaser=data["json"], rates=[], comments=[], blocked=False)
+
+            level.save(force_insert=True)
+
+            response = jsonify({"return_code": 200, "message": "OK", "id_level": id_level, "json": data["json"]}), 200
+        except:
+            pass
+
+    return response
 
 @levels.route('/commentLevel', methods=['PUT'])
 def commentLevel():
