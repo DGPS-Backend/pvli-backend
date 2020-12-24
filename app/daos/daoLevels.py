5ebbe1ac5f1ea39c7eca29bfc3cb7a7940006190
borjawer <74541082+transexylvania@users.noreply.github.com>
2020-12-24 11:25:07 +0100

    modelo de su puta madre el dao

--- app/daos/daoLevels.py
+++ app/daos/daoLevels.py
@@ -1,14 +1,17 @@
 import mongoengine as me
 
 class Level(me.Document):
-    id_level = me.StringField(primary_key=True)
-    name_level = me.StringField(required=True)
-    json_phaser = me.StringField(required=True)
-    # Media de las puntuaciones de todos los usuarios
-    rate = me.StringField(required=True)
-    # Número de usuarios que han puntuado el nivel (para actualizar el valor anterior)
-    rate_count = me.StringField(required=True)
+    id = me.StringField(primary_key=True)
+    name = me.StringField(required=True)
+    image = me.StringField(required=False)
+    # Usuario que lo crea
+    userName = me.StringField(required=False)
     # Comentarios de todos los usuarios
     comments = me.StringField(required=True)
     # Almacena si el nivel ha sido bloqueado
     blocked = me.StringField(required=True)
+    # Media de las puntuaciones de todos los usuarios
+    rating = me.StringField(required=True)
+    # Número de usuarios que han puntuado el nivel (para actualizar el valor anterior)
+    # rate_count = me.StringField(required=True)
+    phaserObject = me.StringField(required=True)
